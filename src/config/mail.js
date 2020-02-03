fe200cee4c65b2ed45a09ad43f8565f468052e96
Bruno Ruggero <37446381+brunoruggero@users.noreply.github.com>
2020-02-02 23:00:40 -0300

    Add files via upload

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth:{
+    user: '80a0e7dd415bab',
+    pass: '50daf4fa7ead57'
+  },
+  default:{
+    from: 'Equipe GoBarber <noreplay@gobarber.com',
+  },
+};
